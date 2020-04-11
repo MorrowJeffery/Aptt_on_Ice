7c3f0cb12155e98fd3a03cb92dceac022ae14628
Jeff Morrow <morrowjefferya@gmail.com>
2020-04-11 11:00:51 -0700

    adjusted members handlebars and added appropriate routes to it

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,8 +9,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      // res.redirect("/index");
-       res.render("members");
+      return res.redirect("/members");
     }
     res.render("signup");
   });
@@ -18,25 +17,28 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.render("members");
+      return res.redirect("/members");
     }
-    res.render("index");
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.render("members");
+    res.render("members", 
+    {
+      memberName: req.user.first_name
+    })
   });
 
   //This route gives the user the page for booking an appointment
   //we will need to adjust this once we have made the handlebar adjustments
-  app.get("/scheduleAppoitment", isAuthenticated, function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/schedule.html"))
+  app.get("/make-reservation", isAuthenticated, function(req,res) {
+    res.render("calendar");
   });
 
   //route for showing any appointments currently booked
-  app.get("/currentAppointments", isAuthenticated, function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/appointments.html"))
+  app.get("/reservations", isAuthenticated, function(req,res) {
+    res.render("reservations");
   })
 };
