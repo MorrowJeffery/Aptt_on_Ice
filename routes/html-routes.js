2ccf8e6808f8309bbe22c85ca6aed2501e24cc7d
Luis Sandoval <luissandoval1126@gmail.com>
2020-04-13 19:02:52 -0700

    Jeever helper object

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
-
+var Jeeves = require("./reservation");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
@@ -25,7 +25,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.render("members", req.user);
+    res.render("members",{});
   });
 
   // Calendar route
